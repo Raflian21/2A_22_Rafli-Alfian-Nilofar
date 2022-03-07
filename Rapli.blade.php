9e79b7cbcd29f4c82615d2776046ba471997dd29
raflian21 <95726593+Raflian21@users.noreply.github.com>
2022-03-07 14:28:14 +0700

    Add files via upload

---
+++ Rapli.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style type="text/css"></style>
+    <link rel="stylesheet" href="css/Rapli.css"></link>
+    <title>Biodata Pribadi</title>
+</head>
+<body>
+    <h1 align="center">Biodata</h1>
+    <hr width="1000" style=2>
+    <table align="center" cellpadding="5" align="center" width="1000">
+        <tr>
+            <td> NIM</td>
+            <td>: 2041720092</td>
+            <td align="center">Nama Mahasiswa : <br>Rafli Alfian Nilofar</td>
+        </tr>
+        <tr>
+            <td> Tempat, Tanggal Lahir</td>
+            <td>: 13 April 2002</td>
+            <td rowspan="5" align="center"><img src="fotoku.jpg" width="200"></td>
+        </tr>
+        <tr>
+            <td> Agama</td>
+            <td>: Islam</td>
+        </tr>
+        <tr>
+            <td> Jenis Kelamin</td>
+            <td>: Laki-laki</td>
+        </tr>
+        <tr>
+            <td> Alamat</td>
+            <td>: Perumahan Ikip Tegalgondo Asri 2D/11</td>
+        </tr>
+        <tr>
+            <td> Usia</td>
+            <td>: 20 Tahun</td>
+        </tr>
+        <tr>
+            <td> Email</td>
+            <td>: raflialfiannilofar@gmail.com</td>
+            <td align="center">No. Handphone : 081907450757</td>
+        </tr>
+    </table>
+    <class name="social">
+        <p align="center"><a href="https://twitter.com/"><img src="twitter.png" width="25" align="center">Rafli</a>
+        <a href="https://www.instagram.com/"><img src="instagram.png" width="25" align="center">Rafli</a></p>
+    </class>
+    <footer>
+        <div class="footer-bottom">
+            <p>copyright &copy;2022 codeOpacity. designed by <span>Rafli</span></p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/welcome.css"></link>
+    <title>Welcome</title>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Welcome</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="keluar" class="tbl-satu">Keluar</a></li>
+                </ul>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <section id="home">
+           <img src="gambar2.png" width=250>
+           <div class="kolom">
+               <p class="deskripsi">Biografi</p>
+               <h2>Rafli Alfian Nilofar</h2>
+               <p>Saya adalah mahasiswa Politeknik Negeri Malang 
+                   Jurusan Tekhnologi Informasi prodi D4 Teknik Informatika.
+                   Saya masuk kuliah angkatan tahun 2020.Saya berasal dari SMAN 9 Malang.
+                   Saya berdomisili Malang sehingga memudahkan saya untuk menjalani pembelajaran mata kuliah di Kampus Polinema.
+                   Jarak rumah saya ke Polinema tidak terlalu jauh yaitu sekitar 30 menit.
+               </p>
+               <p><a href="Rapli" class="tbl-dua">Biodata</a></p>
+           </div> 
+        </section>
+    </div>
+</body>
+</html>
